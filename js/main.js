7a77d635ba4199966a8c6244e2e78ad57f102986
Matthew Speiser <mattrspeiser@gmail.com>
2017-08-18 18:31:15 -0700

    added box shadow, looks so much better

--- js/main.js
+++ js/main.js
@@ -1,32 +1,30 @@
 (function(){
 
-	(function(){
-	
-	var Validator = {
-		textString : "Password111",
-		lessThan6Char : "Your password needs at least 6 characters!",
-		noNumberWarn : "Your password needs at least 1 number!",
-		passwordValidated : "Your password is OK",
 
-		displayText : function(){
-			$('#password').on('keyup', function(e){
-				var txtInput = $(this).val();
-				// console.log(txtInput);
-				// $(".displayPass").text(txtInput);
-		       	if(txtInput.length > 5 && txtInput.match(/\d/)){
-		       		$('.displayValidation').text(Validator.passwordValidated);
-		       		$('.displayValidation2').text(Validator.passwordValidated);
-		       		$(':input[type="submit"]').prop('disabled', false);
-		       	} else {
-		       		$(".displayValidation").text(Validator.lessThan6Char);
-		       		$(".displayValidation2").text(Validator.noNumberWarn);
-		       		
-		       	}
-		    });
-		}
+var Validator = {
+	textString : "Password111",
+	lessThan6Char : "Your password needs at least 6 characters!",
+	noNumberWarn : "Your password needs at least 1 number!",
+	passwordValidated : "Your password is OK",
+
+	displayText : function(){
+		$('#password').on('keyup', function(e){
+			var txtInput = $(this).val();
+			// console.log(txtInput);
+			// $(".displayPass").text(txtInput);
+	       	if(txtInput.length > 5 && txtInput.match(/\d/)){
+	       		$('.displayValidation').text(Validator.passwordValidated);
+	       		$('.displayValidation2').text(Validator.passwordValidated);
+	       		$(':input[type="submit"]').prop('disabled', false);
+	       	} else {
+	       		$(".displayValidation").text(Validator.lessThan6Char);
+	       		$(".displayValidation2").text(Validator.noNumberWarn);
+	       		
+	       	}
+	    });
+	}
 }
 Validator.displayText();
-})();
 
 
 
